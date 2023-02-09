e2a736a658de2af49daf29c9b349e6994ab740b4
Salah <salah@louizy.com>
2023-02-09 10:34:33 +0100

    updated hover border color to purple-600

--- js/components/emojis.js
+++ js/components/emojis.js
@@ -2,7 +2,8 @@ export default {
   template: `
   <div class="relative m-8">
     <div style="transform-box: fill-box;" class="border border-gray-400 rounded p-4 
-    z-0 absolute origin-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 hover:cursor-default hover:scale-125 hover:border-2 hover:text-2xl hover:z-10">
+    z-0 absolute origin-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
+    hover:cursor-default hover:scale-125 hover:border-2 hover:text-2xl hover:z-10 hover:border-purple-600">
       {{emoji.emoji}}
     </div>
   </div>
